b7628ee39f566705fce272cb8eed12fa20eb5768
yevhensydorov <assid@ukr.net>
2018-01-19 13:11:12 +0000

    add each loop

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) => res.render("home", { title: "Test title", name: "Eugene", dataArray: ["olena", "michael", "george"] }));
 
 
 app.listen(3000, () => console.log("Server has started!!!"));
